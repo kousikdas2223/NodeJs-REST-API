3f8972279eb87537f03367cc07ee0398c825f401
Kousik Das <das,kousik2223@gmail.com>
2024-12-26 09:59:27 +0530

    12-25-2024 11:29 PM Commi

--- controllers/feed.js
+++ controllers/feed.js
@@ -2,20 +2,26 @@ exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
       {
+        _id: '1',
         title: "First Post",
         content: "This is the first post from Get method!",
+        imageUrl: 'images/snail.jpg',
+        creator: {
+          name: 'Kousik Das'
+        },
+        createdAt: new Date()
       }
     ]
   });
 };
 
-exports.postPosts = (req, res, next) => {
+exports.createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
   res.status(201).json({
         message: "Created from the post request",
       post: {
-          id: new Date().toISOString(),
+          _id: '2',
           title: title,
           content: content
       }
--- routes/feed.js
+++ routes/feed.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const controllers = require('../controllers/feed');
-
+const {check, body} = require('express-validator');
 const router = express.Router()
 
 router.get('/posts', controllers.getPosts);
-router.post('/post', controllers.postPosts);
+router.post('/post', [
+    body('title')
+        .trim()
+        .isLength({ min: 5 }),
+    body('content')
+        .trim()
+        .isLength({min: 5})
+], controllers.createPost);
 
 module.exports = router;
